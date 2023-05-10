79c55e1c00c9e4da94e659763ff45308847bedff
woefoe <132192012+woefoe@users.noreply.github.com>
2023-05-10 19:24:10 +0200

    Update fotkalogger.sh
    Now supports additional logname, for logging multiple sources

--- fotkalogger.sh
+++ fotkalogger.sh
@@ -1,32 +1,42 @@
-#!/bin/sh
+#!/bin/bash
 
 logs_dir="/data/bak/fotka"
 traces_dir="/tmp/fotka"
-log_path="$logs_dir/$(date '+%Y%m%d').txt"
-traces_path="$traces_dir/traces.txt"
-owner=$(whoami)
+owner="$(whoami)"
 
-strip_date() { cut -d $'\t' -f 2-; }
+get_logname () { cut -d $'\t' -f 1; }
+get_date () { cut -d $'\t' -f 2; }
+get_login_and_message () { cut -d $'\t' -f 3,4; }
+
+get_log_path () { echo "${logs_dir}/$(date '+%Y%m%d').$1.txt"; }
+get_traces_path () { echo "${traces_dir}/$1.trace.txt"; }
+is_older_than () { [ "$(stat --format=%Y "$1")" -le "$(( $(date +%s) - $2 ))" ]; }
 
 mkdir -p "$logs_dir" "$traces_dir"
 
 traces_max=20
-# if log already exists, let's initialize buffer with it
-if [ -f "$log_path" ]; then
-    tail -n "$traces_max" "$log_path" | strip_date > "$traces_path"
-fi
 
-touch "$log_path" "$traces_path"
-chown $owner:$owner "$logs_dir" "$traces_dir" "$log_path" "$traces_path"
+chown "$owner":"$owner" "$logs_dir" "$traces_dir"
 
 # while listening on socket, process the lines
 websocat -s 8085 | while read -r line; do
+    logname=$(echo "$line" | get_logname)
+    log_path=$(get_log_path "$logname")
+    traces_path=$(get_traces_path "$logname")
+
+    # if log already exists and is and trace doesn't or is old, let's initialize buffer with it's
+    # last few lines
+    if [ -f "$log_path" ] && { ! [ -f "$traces_path" ] || is_older_than "$log_path" 600; }; then
+        tail -n "$traces_max" "$log_path" | get_login_and_message > "$traces_path"
+    fi
 
     # we drop timestamp, because it's client that adds it
-    stripped_line=$(echo "$line" | strip_date)
+    stripped_line=$(echo "$line" | get_login_and_message)
 
     # drop lines that are repeated in traces file
-    if grep -F "$stripped_line" "$traces_path"; then
+    if ! [ -f "$traces_path" ]; then
+        touch traces_path
+    elif grep -F "$stripped_line" "$traces_path"; then
         continue
     fi
 
@@ -36,6 +46,8 @@ websocat -s 8085 | while read -r line; do
         sed -i 1d "$traces_path"
     fi
 
+    date=$(echo "$line" | get_date)
+
     echo "$stripped_line" >> "$traces_path"
-    echo "$line" >> "$log_path"
+    printf "%s\t%s\n" "$date" "$stripped_line" >> "$log_path"
 done
