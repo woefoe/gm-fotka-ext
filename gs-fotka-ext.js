a2eb442d8b7a4ec81a988e1c199cebbe5571bc1b
woefoe <132192012+woefoe@users.noreply.github.com>
2023-05-11 09:07:01 +0200

    Major refactor, live chat logging
    * added dynamic loading of jQuery
    * added live chat message logging
    * simplified code with use of matches_any_pattern

--- gs-fotka-ext.js
+++ gs-fotka-ext.js
@@ -16,6 +16,7 @@ let cfg = {
   gwiazdaScale: 0.6,                        // scale of a star icon in chat
   blockInterval: 1000,                      // interval of filtering messages and users
   logUsersInterval: 30000,                  // interval of user logging
+  liveChatInterval: 1000,                   // interval on processing live chat messages
   hidePopupMessages: true,                  // hide popups about swearing in chat
   hideSimpButtons: true,                    // hide buttons for simps like "Tak i Nie", "Ranking"
   hideUsersChatMessages: true,              // hide messages list in chat
@@ -55,6 +56,23 @@ let messagesQueue = []
 const messageRegexp = /([\w\(\)]+:\n?)(.*)$/
 
 // helper functions
+function load_script(url, callback)
+{
+    // adding the script element to the head as suggested before
+   var head = document.getElementsByTagName('head')[0];
+   var script = document.createElement('script');
+   script.type = 'text/javascript';
+   script.src = url;
+
+   // then bind the event to the callback function 
+   // there are several events for cross browser compatibility
+   script.onreadystatechange = callback;
+   script.onload = callback;
+
+   // fire the loading
+   head.appendChild(script);
+}
+
 function get_timestamp() {
   let date = new Date()
   let dateStr = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
@@ -87,7 +105,7 @@ function add_message(logname, date, login, message) {
 
 function mark_message_as_mention(target) {
   var mentionClass = "message-to-me";
-  target.children(".cloud-arrow-border").addClass(mentionClass)
+  target.find(".cloud-arrow-border").addClass(mentionClass)
   target.find(".cloud-arrow-border-arrow").addClass(mentionClass)
   target.find(".cloud-container-body").addClass(mentionClass)
 }
@@ -104,19 +122,38 @@ function log_chat_users() {
   add_message(get_logname() + ".users", get_timestamp(), "_", users)
 }
 
+function matches_any_pattern(patterns, value) {
+  for (var i = 0; i < patterns.length; ++i) {
+    var matches = value.match(patterns[i])
+    if (matches) {
+      return matches
+    }
+  }
+  return null
+}
+
+function get_logname() {
+  var matched = matches_any_pattern(lognamePatterns, location.pathname)
+  if (matched) {
+    return matched[1] + '.' + matched[2]
+  }
+  return "czat.Glowny"
+}
+
 // handlers
 function dom_userlist_handler(event) {
   // this doesn't quite seem to work :)
   var $target = $(event.target)
-  var login = $(event.target).children(".user-login")[0].textContent
-  var login_lower = login.toLowerCase().trim();
-  for (var i = 0; i < blockedUsers.length; ++i) {
-    var userPattern = blockedUsers[i];
-    var matches = userPattern.match(login_lower)
-    if (matches) {
-      console.log("Hiding user " + userPattern + " from chat userlist")
-      $target.addClass(hideClass)
-    }
+  var $login = $target.find(".user-login")[0]
+  if (typeof $login === "undefined") {
+    return
+  }
+  var login = $login.textContent
+  var loginLower = login.toLowerCase().trim();
+
+  if (matches_any_pattern(blockedUsers, loginLower)) {
+    console.log("Hiding user " + loginLower + " from chat userlist")
+    $target.addClass(hideClass)
   }
 
   // apply user name aliases
@@ -133,18 +170,6 @@ function dom_popup_handler(event) {
   }
 }
 
-function get_logname() {
-  let logname = "czat.Glowny"
-  for (var i = 0; i < lognamePatterns.length; ++i) {
-    let lognamePattern = lognamePatterns[i]
-    let matched = location.pathname.match(lognamePattern)
-    if (matched !== null) {
-      logname = matched[1] + '.' + matched[2]
-    }
-  }
-  return logname
-}
-
 function dom_chat_handler(event) {
   let $target = $(event.target)
   let login = $target.attr("login")
@@ -155,75 +180,13 @@ function dom_chat_handler(event) {
   add_message(logname, get_timestamp(), login, message)
 
   // handle mentions
-  for (var i = 0; i < mentionPatterns.length; ++i) {
-    var mentionPattern = mentionPatterns[i]
-    if (message.search(mentionPattern) !== -1) {
-      mark_message_as_mention($target)
-    }
+  if (matches_any_pattern(mentionPatterns, message)) {
+    mark_message_as_mention($target)
   }
 
   // apply user name aliases
   if (login in userAliases) {
     $target.find(".cloud-container-body-login").text(userAliases[login])
-  }
-}
-
-function on_doc_ready(){
-  if (document.domain.indexOf("fotka.com") !== -1) {
-    apply_css()
-
-    if (cfg.hidePopups) {
-      $("body").on("DOMNodeInserted", "body > div", dom_popup_handler)
-    }
-
-    if (cfg.filterUsersInChatUsersOnDom) {
-      $("#users-list").on("DOMNodeInserted", ".chat-user", dom_userlist_handler)
-    }
-
-    if (cfg.logChatUrl.length > 0) {
-      $("#chat #message-box").on("DOMNodeInserted", ".cloud", dom_chat_handler)
-    }
-
-    // TODO color picker with your own colors
-    //      $('#colorPicker').addElement(`
-    //  <input id="colorPickerField" />
-    //      `).onChange(
-    //          //verify it's color, update
-    //          //FotkaChat.ColorPicker.colorSelect(this)
-    //      )
-
-    var intervalTarget
-    if (unsafeWindow === null) {
-      intervalTarget = window;
-    } else {
-      intervalTarget = unsafeWindow;
-    }
-
-    var filterBlockedIntervalId = intervalTarget.setInterval(function () { filter_blocked(); }, cfg.blockInterval)
-    GM_setValue("filterBlockedInterval", filterBlockedIntervalId);
-    var logUsersIntervalId =  intervalTarget.setInterval(function () { log_users(); }, cfg.logUsersInterval)
-    GM_setValue("logUsersInterval", logUsersIntervalId);
-
-    // TODO working hiding notiffications
-    //$("body").bind("DOMNodeInserted", function() {
-    //  child = $("body > div:last-of-type")
-    //  id = child.attr("id")
-    //  if ((typeof(id) == "undefined") || (id.match("colorPicker.*") > 0)) {
-    //    console.log(child.html())
-    //  }
-    //});
-  }
-}
-
-function remove_user_from_live_chat(login) {
-  var chatNode = $("#streams > div > :nth-child(2) > :nth-child(2) > div > div")
-  for (var j=0; j < chatNode.length; ++j) {
-    var child = chatNode.children[j];
-    if (child.textContent.toLowerCase().match("\s*" + login.toLowerCase()).length > 0 &&
-        !($(child).hasClass(hideClass))) {
-      console.log("Hiding message from user " + login + " from stream messages")
-      $(child).addClass(hideClass)
-    }
   }
 }
 
@@ -235,11 +198,12 @@ function remove_user_from_chat(login) {
       }`)
 }
 
-function remove_user_from_chat_users(login) {
+function filter_chat_users() {
   var userNodes = $("#users-list > * > div.chat-user .user-login")
   for (var j = 0; j < userNodes.length; ++j) {
     var userNode = userNodes[j];
-    if (userNode.textContent.toLowerCase().match("^" + login.toLowerCase() + "$") &&
+    var login = userNode.textContent.toLowerCase()
+    if (matches_any_pattern(blockedUsers, login) &&
         !($(userNode).parent().hasClass(hideClass))) {
       console.log("Hiding user " + login + " from user list")
       $(userNode).parent().addClass(hideClass)
@@ -247,61 +211,62 @@ function remove_user_from_chat_users(login) {
   }
 }
 
-function filter_blocked() {
-  if (typeof jQuery === 'undefined') {
-    // If JQuery is not available, there's no frequently updated content
-    console.log("jQuery is not available, filtering inactive");
-
-    // stop loop if there is one
-    var id = GM_getValue("filterBlockedInterval", null)
-    if (id > 0) {
-      console.log("Stopping loop")
-      if (unsafeWindow === null) {
-        window.clearInterval(id);
-      } else {
-        unsafeWindow.clearInterval(id)
-      }
-      GM_setValue("filterBlockedInterval", 0)
-    }
-    return;
+function get_live_chat_messages() {
+  var chatNodes = $("#streams > div > :nth-child(2) > :nth-child(2) > div > div > div")
+  var chatMessages = []
+  for (var i = 0; i < chatNodes.length; ++i) {
+    var $node = $(chatNodes[i]);
+    var $subnode = $node.find("> div > div:nth-child(1) > div:nth-child(2)")
+    var login = $subnode.find("> div:nth-child(1) > div").text().trim()
+    var message = $subnode.find("> div:nth-child(2) > div").text().trim()
+    chatMessages.push({ login: login, message: message, node: $node })
   }
-
-  for (var i=0; i < blockedUsers.length; ++i) {
-    var userPattern = blockedUsers[i]
-
-    // Filter chat in streams
-    if (cfg.filterUsersInLiveChat) {
-      remove_user_from_live_chat(userPattern)
-    }
-
-    // Filter user list in chat
-    if (cfg.filterUsersInChatUsersInterval) {
-      remove_user_from_chat_users(userPattern)
-    }
-  }
+  return chatMessages;
 }
 
-function log_users() {
-  if (typeof jQuery === 'undefined') {
-    // If JQuery is not available, there's no frequently updated content
-    console.log("jQuery is not available, filtering inactive");
+function process_live_chat() {
+  var oldMessages = []
+  var curMessages = get_live_chat_messages()
+  var newMessages = []
 
-    // stop loop if there is one
-    var id = GM_getValue("logUsersInterval", null)
-    if (id > 0) {
-      console.log("Stopping loop")
-      if (unsafeWindow === null) {
-        window.clearInterval(id);
-      } else {
-        unsafeWindow.clearInterval(id)
-      }
-      GM_setValue("logUsersInterval", 0)
-    }
-    return;
+  if (localStorage.liveChatMessages) {
+    oldMessages = JSON.parse(localStorage.liveChatMessages)
   }
 
-  if (location.pathname.startsWith('/czat')) {
-    log_chat_users()
+  // current messages not in old messages are new messages
+  for (var i = 0; i < curMessages.length; ++i) {
+    var curMessage = curMessages[i]
+    if (!oldMessages.find(oldMessage => oldMessage.message == curMessage.message)) {
+      newMessages.push(curMessage)
+    }
+  }
+
+  for (var i = 0; i < newMessages.length; ++i) {
+    newMessage = newMessages[i]
+    // we probably want to log new messages...
+    add_message(get_logname(), get_timestamp(), newMessage.login, newMessage.message)
+
+    // and then filter out the ones we don't want
+    if (cfg.filterUsersInLiveChat) {
+      if (matches_any_pattern(blockedUsers, newMessage.login)) {
+        console.log("Hiding message from user " + newMessage.login + " from stream messages")
+        newMessage.node.addClass(hideClass)
+      }
+    }
+  }
+
+  // nodes can't be stringified, so we clear them
+  for (var i = 0; i < curMessages.length; ++i) {
+    curMessage = curMessages[i]
+    delete curMessage.node
+  }
+
+  localStorage.liveChatMessages = JSON.stringify(curMessages)
+}
+
+function filter_blocked() {
+  if (cfg.filterUsersInChatUsersInterval) {
+    filter_chat_users()
   }
 }
 
@@ -342,6 +307,70 @@ function apply_css() {
       remove_user_from_chat(userPattern)
     }
   }
+}
+
+function on_doc_ready() {
+  if (document.domain.indexOf("fotka.com") === -1) {
+    return
+  }
+
+  if (typeof jQuery === 'undefined') {
+    load_script('https://code.jquery.com/jquery-3.6.4.slim.js', initialize)
+  } else {
+    initialize()
+  }
+}
+
+function initialize() {
+  apply_css()
+
+  if (cfg.hidePopups) {
+    $("body").on("DOMNodeInserted", "body > div", dom_popup_handler)
+  }
+
+  // TODO color picker with your own colors
+  //      $('#colorPicker').addElement(`
+  //  <input id="colorPickerField" />
+  //      `).onChange(
+  //          //verify it's color, update
+  //          //FotkaChat.ColorPicker.colorSelect(this)
+  //      )
+
+  var intervalTarget
+  if (unsafeWindow === null) {
+    intervalTarget = window
+  } else {
+    intervalTarget = unsafeWindow
+  }
+
+  if (location.pathname.startsWith('/kamerka')) {
+    intervalTarget.setInterval(process_live_chat, 1000)
+    intervalTarget.setInterval(process_live_chat, cfg.liveChatInterval)
+  }
+
+  if (location.pathname.startsWith('/czat')) {
+    if (cfg.filterUsersInChatUsersOnDom) {
+      $("#users-list").on("DOMNodeInserted", ".chat-user", dom_userlist_handler)
+    }
+
+    if (cfg.logChatUrl.length > 0) {
+      $("#chat #message-box").on("DOMNodeInserted", ".cloud", dom_chat_handler)
+    }
+
+    intervalTarget.setTimeout(filter_blocked, 1000)
+    intervalTarget.setTimeout(log_chat_users, 1000)
+    intervalTarget.setInterval(filter_blocked, cfg.blockInterval)
+    intervalTarget.setInterval(log_chat_users, cfg.logUsersInterval)
+  }
+
+  // TODO working hiding notiffications
+  //$("body").bind("DOMNodeInserted", function() {
+  //  child = $("body > div:last-of-type")
+  //  id = child.attr("id")
+  //  if ((typeof(id) == "undefined") || (id.match("colorPicker.*") > 0)) {
+  //    console.log(child.html())
+  //  }
+  //});
 }
 
 if (document.readyState === "complete" || (document.readyState !== "loading" && !document.documentElement.doScroll)) {
